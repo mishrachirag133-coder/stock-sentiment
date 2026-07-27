f01d828c77e840fc894d560ad618e1397f4fc1c3
mishrachirag133-coder <mishrachirag133@gmail.com>
2026-07-27 14:56:10 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,9 +2,8 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-
+from tf_keras.models import Sequential
+from tf_keras.layers import LSTM, Dense
 def get_stock_data(stock_name, interval="1h"):
     ticker = stock_name.upper() + ".NS"
     
